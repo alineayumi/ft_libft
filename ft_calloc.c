bbf824cdd877baac881a870892b60dca25aa8698
alineayumi <alineayumi@gmail.com>
2020-04-12 21:27:21 -0300

    malloc functions

---
+++ ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afukuhar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/12 20:50:25 by afukuhar          #+#    #+#             */
+/*   Updated: 2020/04/12 21:11:24 by afukuhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (!(ptr = malloc(size * count)))
+		return (NULL);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
